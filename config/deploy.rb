bffc4f9849af3e1de2fa919888a397eee174d9ce
alaraby-tv <pmarkowski@alaraby.tv>
2020-05-22 20:58:28 +0100

    add database yaml to shared config

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,6 @@ server '192.168.1.32', port: 22, roles: [:web, :app, :db], primary: true
 set :application, "appraisal"
 set :repo_url, "git@github.com:alaraby-tv/appraisal.git"
 set :user, 'deploy'
-set :linked_files, %w{config/master.key}
 set :puma_threads, [4, 16]
 set :puma_workers, 0
 
@@ -35,7 +34,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/master.key}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
