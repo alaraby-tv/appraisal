1ee5f591507e4b7f8342b32b30239cffe7b8c710
alaraby-tv <pmarkowski@alaraby.tv>
2020-06-01 10:45:06 +0100

    before update calculate score

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,4 +1,13 @@
 class Evaluation < ApplicationRecord
+  before_update :calculate_score
   belongs_to :user, inverse_of: :evaluations
   belongs_to :objective, inverse_of: :evaluations
+
+  validates :score, inclusion: 1..10, on: :update
+
+
+  def calculate_score
+    calculated = score * objective.objective_percentage / 10
+    self.calculated_score = calculated
+  end
 end
