33b4cedad8ce9836d4661afee5e196f8d4bd0de9
alaraby-tv <pmarkowski@alaraby.tv>
2020-06-01 10:46:08 +0100

    before save update total percentage of the group and the section

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,21 +1,29 @@
 class Objective < ApplicationRecord
-  before_create :add_total_percentage_to_the_section
+  before_save :update_total_percentage_of_the_section
   belongs_to :section, inverse_of: :objectives
   has_many :evaluations, inverse_of: :objective, dependent: :destroy
+  has_many :users, through: :evaluations, inverse_of: :objectives
   has_one :group, through: :section
 
   validates :name, :section, presence: true
   validates_uniqueness_of :name, scope: :section
   validate :total_section_percentage_cannot_be_greater_than_specified
 
-  def add_total_percentage_to_the_section
-    updated_section_percentage = section.total_section_percentage + objective_percentage
+  private
+
+  def new_percentage
+    previous_objective_percentage = objective_percentage_was == nil ? 0.0 : objective_percentage_was
+    new_objective_percentage = objective_percentage - previous_objective_percentage
+  end
+
+  def update_total_percentage_of_the_section
+    updated_section_percentage = section.total_section_percentage + new_percentage
     section.update_attribute(:total_section_percentage, updated_section_percentage)
   end
 
   def total_section_percentage_cannot_be_greater_than_specified
-    if section.total_section_percentage + objective_percentage > section.section_percentage
-      errors.add(:objective_percentage, "can't be more than #{section.section_percentage.to_i - section.total_section_percentage.to_i}")
+    if section.total_section_percentage + new_percentage > section.section_percentage
+      errors.add(:objective_percentage, "exceeds allowed section total percentage (#{section.section_percentage}%)")
     end
   end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,22 +1,34 @@
 class Section < ApplicationRecord
-  before_save :add_total_percentage_to_the_group
+  before_save :update_total_percentage_of_the_group
   belongs_to :group, inverse_of: :sections
   has_many :objectives, inverse_of: :section, dependent: :destroy
 
   validates :name, :group, presence: true
   validates_uniqueness_of :name, scope: :group
   validate :total_group_percentage_cannot_be_greater_than_a_hundred
+  validate :section_percentage_cannot_be_lower_than_total_percentage
 
   private
 
-  def add_total_percentage_to_the_group
-    updated_group_percentage = group.total_group_percentage + section_percentage
+  def new_percentage
+    previous_section_percentage = section_percentage_was == nil ? 0.0 : section_percentage_was
+    new_section_percentage = section_percentage - previous_section_percentage
+  end
+
+  def update_total_percentage_of_the_group
+    updated_group_percentage = group.total_group_percentage + new_percentage
     group.update_attribute(:total_group_percentage, updated_group_percentage)
   end
 
   def total_group_percentage_cannot_be_greater_than_a_hundred
-    if group.total_group_percentage + section_percentage > 100.0
-      errors.add(:section_percentage, "can't be more than #{100 - group.total_group_percentage.to_i}")
+    if group.total_group_percentage + new_percentage > 100
+      errors.add(:section_percentage, "exceeds allowed total group percentage (100.0%)")
+    end
+  end
+
+  def section_percentage_cannot_be_lower_than_total_percentage
+    if section_percentage < total_section_percentage
+      errors.add(:section_percentage, "can't be lower than total percentage of its objectives (#{total_section_percentage}%)")
     end
   end
 end
