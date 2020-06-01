88bdc488ef8d0179da627b49dde5c7065a8b1045
alaraby-tv <pmarkowski@alaraby.tv>
2020-06-01 10:44:01 +0100

    after save set objectives

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_save :set_objectives
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,8 +7,22 @@ class User < ApplicationRecord
 
   belongs_to :group, inverse_of: :users, optional: true
   has_many :evaluations, inverse_of: :user, dependent: :destroy
+  has_many :objectives, through: :evaluations, inverse_of: :users
 
   def name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def set_objectives
+    if group
+      unless objectives.ids.sort == group.objectives.ids.sort
+        evaluations.each {|e| e.destroy}
+        group.objectives.each do |objective|
+          evaluations.create(objective: objective)
+        end
+      end
+    end
   end
 end
